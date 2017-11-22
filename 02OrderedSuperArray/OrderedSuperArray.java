c55c65ab83770bb66deb770b32357ee0a0843ed2
Chicken-Ninja <eko1@stuy.edu>
2017-11-22 11:41:43 -0500

    Added OrderedSuperArray and it's SuperArray friend

---
+++ 02OrderedSuperArray/OrderedSuperArray.java
@@ -0,0 +1,85 @@
+public class OrderedSuperArray extends SuperArray{
+     /*constructor initializes an empty List*/
+  public OrderedSuperArray(int StartingCapacity){
+      super(StartingCapacity);
+ //Complete this.
+
+  }
+
+  /*call the proper add.*/
+  public void add(int index, String value){
+    add(value);
+  }
+
+    private  int find(String value){
+	if (size() == 0) {return 0;}
+	for (int counter = 0 ; counter < size(); counter ++ ){
+
+	    if(get(counter).compareTo(value) > 0) {return counter;}
+	    if(get(counter).compareTo(value) == 0) {return counter;}
+	}
+	return size();}
+    private int findBinary(String value) {
+	if(size() == 0) {return 0;}
+	if(get(size() / 2) .compareTo(value) ==  0){return size() / 2;}
+	if(get(size()/ 2) .compareTo(value) > 0) {
+	    for (int counter = size()/2 ; counter > 0; counter --){
+		if(get(counter).compareTo(value) < 0) {return counter + 1;}
+	    }
+	    return 0;
+	}
+	else/* if(get(size() / 2).compareTo(value) < 0)*/{
+		for(int counter = size() / 2; counter < size(); counter ++ ) {
+		    if (get(counter).compareTo(value) > 0) {return counter - 1;}}
+		return size();}
+    }
+     public int lastIndexOfBinary(String element){
+	 int start = indexOfBinary(element);
+	int finish = size();
+	while (start != finish){
+	    if (get((start + finish)/2).compareTo(element) > 0){
+		finish = (start + finish) / 2;}
+	    else if (get((start + finish)/2).equals(element)){
+		if (start == size() - 1 || !(get((start + finish) / 2 + 1).equals(element))){
+		    return (start + finish) / 2;
+		}
+		finish = (start + finish) / 2;
+		start = (start + finish) / 2 - 1; 
+	    }else {
+		start = (start + finish) / 2 + 1;
+	    }}
+	if (get(start) != element){
+	    return -1;}
+	return start;}
+    
+     public int indexOfBinary(String element){
+	 int start = 0;
+	int finish = size();
+	while (start != finish){
+	    if (get((start + finish)/2).compareTo(element) > 0){
+	        finish = (start + finish) / 2;
+	    } else if (get((start + finish)/2).equals(element)){
+		if (start == 0 || !(get(((start + finish) / 2) - 1).equals(element))){
+		    return (start + finish) / 2;}
+		finish = (start + finish) / 2;
+		start = (start + finish) / 2 - 1; 
+	    }else {
+		start = (start + finish) / 2 + 1;
+	    }}
+	if (get(start) != element){
+	    return -1;}
+	return start;
+    }	
+
+       
+  /*Write this method and any
+    appropriate helper methods.*/
+  public boolean add(String value){
+      int place = findBinary(value);
+      super.add(place , value);
+      return true;}
+ 
+	
+}
+
+
