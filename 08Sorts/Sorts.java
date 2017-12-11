30427e91bbb15c48fb2ca9158330367caafa1e36
Chicken-Ninja <eko1@stuy.edu>
2017-12-10 21:34:08 -0500

    Fixed my sorts, and added a git ignore

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -2,6 +2,24 @@ import java.util.Arrays;
 public class Sorts {
     private int Position = 0; 
 
+    public static void Interval(int[] ary) {
+	for(int counter = 0; counter < ary.length; counter++ )
+	    {positionless(ary , counter);}
+    
+    }
+    
+    public static void positionless(int[] ary, int start) {
+	int smallest = ary[start];
+	int position = 0; 
+	
+	for(int counter = start; counter > 0; counter --){
+	    if(ary[counter] < ary[counter - 1]) {swap(ary , counter, counter - 1);}
+	}
+    }
+   
+	   
+    
+
     public static void swap(int[]ary , int a , int b) {
 	int c = ary[a];
 	ary[a] = ary[b];
@@ -28,6 +46,6 @@ public class Sorts {
 	    }
 	
     public static String name() {
-	return "01.Last.First";}
+	return "10.Ko.Ethan";}
 }
     
