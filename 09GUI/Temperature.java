352d3b1910a1b7a92e18584f687594210e7573e6
Chicken-Ninja <eko1@stuy.edu>
2017-12-15 00:07:06 -0500

    made the window work and added all the buttons , checkboxes. and textfields, Working code

--- 09GUI/Temperature.java
+++ 09GUI/Temperature.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Temperature extends JFrame {
+public class Temperature extends JFrame implements ActionListener{
     private Container pane; 
-    private JCheckBox A;
-    private JTextField t; 
+    private JCheckBox A , B ;
+    private JTextField t,c;
+    private JButton Submit;
     
     
 
@@ -17,16 +19,44 @@ public class Temperature extends JFrame {
 	pane.setLayout(new FlowLayout());
 
 	A = new JCheckBox("Celcius to Fahrenheit");
-
+	B = new JCheckBox("Fahrenheit to Celcius"); 
+	c = new JTextField(15);
 	t = new JTextField(15); 
+	Submit = new JButton("Submit");
 
-	pane.add(A);
+	A.addActionListener(this);
+	B.addActionListener(this);
+	t.addActionListener(this);
+	c.addActionListener(this);
+	Submit.addActionListener(this);
+	pane.add(Submit);
+	pane.add(B);
 	pane.add(t);
+	pane.add(A);
+	pane.add(c);
     }
 
-    //public void actionPerformed(ActionEvent e) {
-    //	String s = e.getActionCommand();
-    //	System.out.println(s);
+    public void actionPerformed(ActionEvent e) {
+    	String s = e.getActionCommand();
+    	System.out.println(s);
+	if(s.equals("Submit"))
+	    {	if(A.isSelected() && B.isSelected() ) {
+		   c.setText("Choose one option!");
+		   }
+		else{
+
+
+		if(A.isSelected() )
+		    {c.setText("" + CtoF(Double.parseDouble(t.getText())));}
+		if(B.isSelected() ) 
+		    {c.setText("" + FtoC(Double.parseDouble(t.getText())));}
+	    		}
+	    }
+    }
+
+
+       
+		    
 	
     
     
@@ -38,9 +68,9 @@ public class Temperature extends JFrame {
     
     
     public static void main(String[] args) {
-	System.out.println(CtoF(-40.0));
-	System.out.println(FtoC(212.0));
-	System.out.println(CtoF(100.0));
+	//System.out.println(CtoF(-40.0));
+	//System.out.println(FtoC(212.0));
+	//System.out.println(CtoF(100.0));
 	Temperature a = new Temperature();
 	a.setVisible(true);
     }
